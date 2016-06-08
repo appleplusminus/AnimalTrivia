db4614fc455e4d3bc45bea6490543ac0da83fe2e
kengo hiwatashi <kengo.hiwatashi@gmail.com>
2016-06-08 20:34:33 +0900

    Connecting Up Our UILabels

--- AnimalTrivia/ViewController.swift
+++ AnimalTrivia/ViewController.swift
@@ -10,6 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var aLabel: UILabel!
+    
+    @IBOutlet weak var bLabel: UILabel!
+    
+    @IBOutlet weak var cLabel: UILabel!
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
